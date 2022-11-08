2b4a1e44dbf599d95a2c1a8c9f4989fabd983d5a
sujala Potula <sujalapotula1@gmail.com>
2022-11-07 18:04:41 -0600

    first commit

---
+++ assessment-js-basics-1/gitInfo.js
@@ -0,0 +1,107 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+
+var gitDefinition = " It is a Global Information Tracker"
+console.log(gitDefinition);
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+
+var gidHubbDefinition =" Github is a web based interface that uses git, the open source version contral aftwr thta lets multiple people make seperate changes to webpages at the same time";
+console.log(gidHubbDefinition);
+
+
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+
+var gidinitDefinition = " gitinit is a command that creates a new git repository)"
+console.log(gidinitDefinition);
+
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+
+var gidCloneDefinition = "Gitclone  is a command to point to an exsisting repo and make a clone or copy of the repo at in a new directory at other location."
+console.log(gidCloneDefinition);
+
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+
+var gidStatusDefinition = "Git Status is a commad that displays the state of the working directory and the staging area";
+console.log(gidStatusDefinition);
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+
+var gidAddDefinition =" GitAdd: It is a  command that adds a change in the workingdirectory to the staging area"
+console.log(gidAddDefinition);
+
+var gitAddCode = "git add ."
+console.log(gitAddCode);
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+
+var gitCommitDefinition = " GITCOMMIT: Its a command used to save your chnages to the local repository"
+console.log(gitCommitDefinition);
+
+var gitcommitcode = " Gitcommitcode:  git commit -m commit message"
+
+console.log(gitcommitcode)
+
+// /PROBLEM 8
+//     Create a variable called 'gitPushDefinition'.  
+//     It should be a string containing your best definition of what 'git push' does.
+// //CODE HERE
+
+var gitPushDefinition =" gitPushcommad: iys a command that allows you to send the commits from your local branch in your local git repository to the remote repository"
+console.log(gitPushDefinition);
+
---
+++ assessment-js-basics-1/sdlc.js
@@ -0,0 +1,48 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+// Problem 1 Answers
+
+// planning
+// Analysis of Requirements
+// Design
+// implementation
+// testing and integration
+// maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "PLANNING:  This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+console.log(planning);
+
+const requirementAnalysis = "Requirement Analysis:   This is the scecond step in which the goal is to be sure all the product requirements accurately represent stakeholder needs and requirements."
+console.log(requirementAnalysis);
+
+const design = "Code Design:  This i sthe third step, in which the software designers will define the technical details of the product."
+console.log(design);
+
+const implementaion = "Implementation:  All the components of the software are implemented and the code is writeen.)"
+console.log(implementaion);
+
+const testingAndintegration = "Testing and Integration: In this phase all the software and hardware configuration item components have been constructed and successfully tested."
+console.log(testingAndintegration);
+
+const maintenance = "Maintenance:  This occurs once the product is in full operation.It includes sftware upgrades,repairs,and fixes."
+console.log(maintenance);
+
